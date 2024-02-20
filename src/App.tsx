c57da819d12f6eec6c8bd1c3cb7b71907021cf95
sanglap13 <sanglapmridha@gmail.com>
2024-02-20 23:25:37 +0530

    feat: add Home Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Home } from './components/pages';
 
 function App() {
   const a = (e: string) => {
     console.log(e);
   };
-  return <div className="App">hi</div>;
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
 }
 
 export default App;
