63babcda9eb804c0aee814c698613cfd3fc94eeb
sanglap13 <sanglapmridha@gmail.com>
2024-02-21 00:34:00 +0530

    refactor: petty change

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,6 @@ import React from 'react';
 import { Home } from './components/pages';
 
 function App() {
-  const a = (e: string) => {
-    console.log(e);
-  };
   return (
     <div className="App">
       <Home />
--- src/components/pages/home/Home.tsx
+++ src/components/pages/home/Home.tsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import React, { useState } from 'react';
 
 const Home = () => {
-  return <div>Home</div>;
+  const [age, setAge] = useState('');
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setAge(event.target.value as string);
+  };
+  return <div></div>;
 };
 
 export default Home;
