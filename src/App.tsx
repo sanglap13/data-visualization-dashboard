6c7c5d9b43b2329a57ff613d2a24a560cf2297f2
sanglap13 <sanglapmridha@gmail.com>
2024-02-20 23:20:12 +0530

    refactor: update prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React from "react";
+import React from 'react';
 
 function App() {
   const a = (e: string) => {
     console.log(e);
   };
-  return <div className="App"></div>;
+  return <div className="App">hi</div>;
 }
 
 export default App;
