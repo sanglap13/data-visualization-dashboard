8f6370dce78b4173c4f4662b20062fb9d92fbcc4
sanglap13 <sanglapmridha@gmail.com>
2024-02-22 12:03:35 +0530

    refactor: push from pc

--- src/components/shared/mainContainer/MainContainer.tsx
+++ src/components/shared/mainContainer/MainContainer.tsx
@@ -107,8 +107,8 @@ const MainContainer = () => {
             ml: { sm: `${drawerWidth}px` },
           }}
         >
-          <Navbar />
-          {/* <Toolbar>
+          {/* <Navbar /> */}
+          <Toolbar>
             <IconButton
               color="inherit"
               aria-label="open drawer"
@@ -121,7 +121,7 @@ const MainContainer = () => {
             <Typography variant="h6" noWrap component="div">
               Responsive drawer
             </Typography>
-          </Toolbar> */}
+          </Toolbar>
         </AppBar>
         <Box
           component="nav"
@@ -162,6 +162,7 @@ const MainContainer = () => {
             {drawer}
           </Drawer>
         </Box>
+        <Toolbar />
         <Box
           component="main"
           sx={{
@@ -170,37 +171,7 @@ const MainContainer = () => {
             width: { sm: `calc(100% - ${drawerWidth}px)` },
           }}
         >
-          <Toolbar />
-          {/* <Typography paragraph>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus
-            dolor purus non enim praesent elementum facilisis leo vel. Risus at
-            ultrices mi tempus imperdiet. Semper risus in hendrerit gravida
-            rutrum quisque non tellus. Convallis convallis tellus id interdum
-            velit laoreet id donec ultrices. Odio morbi quis commodo odio aenean
-            sed adipiscing. Amet nisl suscipit adipiscing bibendum est ultricies
-            integer quis. Cursus euismod quis viverra nibh cras. Metus vulputate
-            eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo
-            quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat
-            vivamus at augue. At augue eget arcu dictum varius duis at
-            consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
-            donec massa sapien faucibus et molestie ac.
-          </Typography>
-          <Typography paragraph>
-            Consequat mauris nunc congue nisi vitae suscipit. Fringilla est
-            ullamcorper eget nulla facilisi etiam dignissim diam. Pulvinar
-            elementum integer enim neque volutpat ac tincidunt. Ornare
-            suspendisse sed nisi lacus sed viverra tellus. Purus sit amet
-            volutpat consequat mauris. Elementum eu facilisis sed odio morbi.
-            Euismod lacinia at quis risus sed vulputate odio. Morbi tincidunt
-            ornare massa eget egestas purus viverra accumsan in. In hendrerit
-            gravida rutrum quisque non tellus orci ac. Pellentesque nec nam
-            aliquam sem et tortor. Habitant morbi tristique senectus et.
-            Adipiscing elit duis tristique sollicitudin nibh sit. Ornare aenean
-            euismod elementum nisi quis eleifend. Commodo viverra maecenas
-            accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam
-            ultrices sagittis orci a.
-          </Typography> */}
+          saddddddddddddddddddddddddddddddd
         </Box>
       </Box>
     </div>
