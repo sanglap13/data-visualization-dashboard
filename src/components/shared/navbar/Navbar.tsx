df8aa4dd7afb07ef829ff8ba7be16de4c01d9dd0
sanglap13 <sanglapmridha@gmail.com>
2024-02-24 21:53:04 +0530

    feat: add basic sidebar

--- src/components/shared/navbar/Navbar.tsx
+++ src/components/shared/navbar/Navbar.tsx
@@ -1,14 +1,119 @@
-import React from 'react';
-import { Box, IconButton, InputBase, useTheme } from '@mui/material';
+import React, { useState } from 'react';
+import {
+  Box,
+  Button,
+  Divider,
+  Drawer,
+  IconButton,
+  InputBase,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Toolbar,
+  Typography,
+  useTheme,
+} from '@mui/material';
 import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined';
 import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
 import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
 import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
 import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
 import SearchIcon from '@mui/icons-material/Search';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+import MenuIcon from '@mui/icons-material/Menu';
+
+import './navbar.css';
+import { SIDEBAR_DETAILS } from '../../../constants/sidebarDetails';
+import { AltorLogo } from '../../../assets';
 
 const Navbar = () => {
-  const theme = useTheme();
+  const [open, setOpen] = useState(false);
+
+  const toggleDrawer = (newOpen: boolean) => () => {
+    setOpen(newOpen);
+  };
+  const DrawerList = (
+    // <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
+    //   <List>
+    //     {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+    //       <ListItem key={text} disablePadding>
+    //         <ListItemButton>
+    //           <ListItemIcon>
+    //             {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+    //           </ListItemIcon>
+    //           <ListItemText primary={text} />
+    //         </ListItemButton>
+    //       </ListItem>
+    //     ))}
+    //   </List>
+    //   <Divider />
+    //   <List>
+    //     {['All mail', 'Trash', 'Spam'].map((text, index) => (
+    //       <ListItem key={text} disablePadding>
+    //         <ListItemButton>
+    //           <ListItemIcon>
+    //             {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+    //           </ListItemIcon>
+    //           <ListItemText primary={text} />
+    //         </ListItemButton>
+    //       </ListItem>
+    //     ))}
+    //   </List>
+    // </Box>
+    <div>
+      <Toolbar />
+
+      <Box display="flex" justifyContent="space-between" alignItems="center">
+        <Typography variant="h4" color="red">
+          Admin Panel
+        </Typography>
+      </Box>
+      <Box mb="25px">
+        <Box display="flex" justifyContent="center" alignItems="center">
+          <img
+            alt="profile-user"
+            width="100px"
+            height="100px"
+            src={AltorLogo}
+            style={{ cursor: 'pointer', borderRadius: '50%' }}
+          />
+        </Box>
+        <Box textAlign="center">
+          <Typography
+            variant="h5"
+            color="red"
+            fontWeight="bold"
+            sx={{ m: '10px 0 0 0' }}
+          >
+            Sanglap Mridha
+          </Typography>
+          <Typography variant="h5" color="red">
+            SDE FE Intern
+          </Typography>
+        </Box>
+      </Box>
+      <Divider />
+
+      <List>
+        {SIDEBAR_DETAILS.map((text, index) => {
+          const { label, redirection_link, icon } = text;
+          return (
+            <ListItem key={label} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  {typeof icon === 'function' ? icon() : icon}
+                </ListItemIcon>
+                <ListItemText primary={label} />
+              </ListItemButton>
+            </ListItem>
+          );
+        })}
+      </List>
+    </div>
+  );
 
   return (
     <div className="navbar">
@@ -18,17 +123,14 @@ const Navbar = () => {
         p={2}
         sx={{ backgroundColor: '#cccccc' }}
       >
-        {/* SEARCH BAR */}
-        <Box
-          display="flex"
-          borderRadius="3px"
-          sx={{ backgroundColor: '#737373' }}
-        >
-          <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
-          <IconButton type="button" sx={{ p: 1 }}>
-            <SearchIcon />
+        <Button className="glow-on-hover" onClick={toggleDrawer(true)}>
+          <IconButton>
+            <MenuIcon />
           </IconButton>
-        </Box>
+        </Button>
+        <Drawer open={open} onClose={toggleDrawer(false)}>
+          {DrawerList}
+        </Drawer>
 
         {/* ICONS */}
         <Box display="flex">
